1758848c95a660874bc39c5110f83df5a3189c64
jojopko <gamerat.github@gmail.com>
2022-11-23 18:27:33 +0300

    add f2 to lab11

--- lab11/main.cpp
+++ lab11/main.cpp
@@ -3,9 +3,12 @@
 #include <stdlib.h>
 #include <math.h>
 
+// вычисление частичной суммы ряда
+// 
 //              -i   1-x
 // Σ(n;i=1) (-1)    -----
 //                   i+1
+// 
 void f1() {
 	int n, step = 1;
 	double x, sum = 0;
@@ -24,10 +27,75 @@ void f1() {
 	printf_s("%lf", sum);
 }
 
-void f2() {
+// приближённое вычисление суммы ряда
+// 
+//                  i
+//  ∞     i  |cos(2x )|
+// Σ  (-1)  ------------
+//  i=1          xi
+//              3
+double f2(double x, double exactness) {
+	int i = 1;
+	double factorial = 1, sum = 0, iter_val;
+	do {
+		iter_val = pow(-1, i) * fabs(cos(2 * pow(x, i))) / pow(3, x * i);
+		sum += iter_val;
+		i++;
+		factorial *= i;
+	} while (fabs(iter_val) > exactness);
+	return sum;
+}
 
+double pow(double n, int x) {
+	double result = 1;
+	for (; x > 0; x--) {
+		result *= n;
+	}
+	return result;
+}
+
+double calc_nearly_sum(double x, double exactness) {
+	int i = 1;
+	double factorial = 1, sum = 0, iter_val;
+	do {
+		iter_val = pow(-1, i) * pow(x, i) / factorial;
+		sum += iter_val;
+		i++;
+		factorial *= i;
+	} while (fabs(iter_val) > exactness);
+	return sum;
+}
+
+void fff(double x, double e) {
+	int st1, i;
+	double s = 0, stx, sl, f;
+	st1 = -1;
+	f = 1;
+	stx = x;
+	i = 1;
+	do {
+		sl = st1 * stx / f;
+		s += sl;
+		i++;
+		stx *= x;
+		st1 *= -1;
+		f *= i;
+	} while (fabs(sl) > e);
+	printf_s("%lf", s);
 }
 
 int main(int argc, char ** argv) {
-	f1();
+	//f1();
+	double x, e;
+	printf_s("x = ");
+	scanf_s("%lf", &x);
+	printf_s("e = ");
+	scanf_s("%lf", &e);
+
+
+	printf_s("%lf\n", f2(x, e));
+	//fff(x, e);
+	//printf_s("%lf", calc_nearly_sum(x, e));
+	
+	return 1;
 }
